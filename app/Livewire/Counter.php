04d2da4c1049df79f3e636b81a599aa4b2744b0b
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-11-19 21:27:59 -0300

    feat: add Page Components

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -1,9 +1,11 @@
 <?php
  
 namespace App\Livewire;
- 
+
+use Livewire\Attributes\Title;
 use Livewire\Component;
  
+#[Title('Counter')]
 class Counter extends Component
 {
     public $count = 0;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\Todos;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',  Todos::class);
+Route::get('/counter',  Counter::class);
